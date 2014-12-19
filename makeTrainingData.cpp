6f415012011b3767b2f1369e40a1aa63401f6686
Maximilian Hemmrich <maximilian.hemmrich@gmail.com>
2014-12-18 23:30:17 -0500

    bugfix

--- makeTrainingData.cpp
+++ makeTrainingData.cpp
@@ -8,7 +8,7 @@ int main() {
    //training set for XOR - if in1 == in2, out = 0. if in1 != in2, out = 1
 
    cout << "topology: 2 4 1" << endl;
-   for(int i = 2000, i >= 0; i--) {
+   for(int i = 2000; i >= 0; i--) {
       int n1 = (int)(2.0 * rand() / double(RAND_MAX));
       int n2 = (int)(2.0 * rand() / double(RAND_MAX));
 
